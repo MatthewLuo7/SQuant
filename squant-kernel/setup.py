45f0dd984c5416c8f71a921ae4384a879ce688c2
Erjing Luo <104494211+MatthewLuo7@users.noreply.github.com>
2026-02-27 12:06:20 -0700

    Rename CUDA extension from quant_cuda to sq_cuda

--- squant-kernel/setup.py
+++ squant-kernel/setup.py
@@ -23,9 +23,9 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='quant_cuda',
+    name='sq_cuda',
     ext_modules=[
-        CUDAExtension('quant_cuda', [
+        CUDAExtension('sq_cuda', [
             'quant.cpp',
             'quant_kernel.cu'],
             include_dirs=['includes'],
